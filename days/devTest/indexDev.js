9541c2e7811cf07de4600735021c1de049cd4da7
Maya <mayabaror@gmail.com>
2023-08-31 16:26:12 +0300

    ss

--- days/devTest/indexDev.js
+++ days/devTest/indexDev.js
@@ -1,13 +1,14 @@
+
 const totalWins = {
     totalBlues: totalBlues,
     totalReds: totalReds
 }
 
 function timeline() {
-    let startDayTwo = async function () {
+    let startDayFour = async function () {
         platform.getAllSessions().then((data) => {
-            getIndexSessionData(data).then((indexI) => {
-                studySessionData = data[indexI];
+            getIndexSessionData(data).then((i) => {
+                studySessionData = data[i];
                 let updatedDates = updateDates();
                 if (updatedDates.fullDate.getDate() == updatedDates.yesterday.getDate()) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {
                     if (window.matchMedia("(orientation: landscape)").matches) {
@@ -19,50 +20,50 @@ function timeline() {
                         moveToDay();
                     }, timeToFive());
                 }
-                else if ((updatedDates.fullDate.getDate() == updatedDates.yesterdayPlusOne.getDate()) && (studySessionData.doneDay3 != "doneDevTest")) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {)
+                else if (updatedDates.fullDate.getDate() == updatedDates.yesterdayPlusOne.getDate() && (studySessionData.doneTest2 != "doneTest2")) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {)
                     if (0 <= updatedDates.fullDate.getHours() & updatedDates.fullDate.getHours() < 5) {
                         document.getElementById("fiveAM").style.display = "inline";
                         setTimeout(() => {
                             moveToDay();
                         }, timeToFiveSameDay());
                     } else {
-                        let goTwo = async function () {
-                            let my_awesome_script = document.createElement('script');
-                            my_awesome_script.setAttribute('src', '../../functions/orientation.js');
-                            // my_awesome_script.src = "../functions/orientation.js";
-                            document.body.appendChild(my_awesome_script);
+                        let goFour = async function () {
                             deleteFromSessionData();
                             document.getElementById("startButton").style.display = "inline";
                             document.getElementById("redButton").style.display = "inline";
                             document.getElementById("blueButton").style.display = "inline";
                             document.getElementById("gameScreen").style.display = "inline";
                             document.getElementById("startButton").onclick = function () {
+                                let my_awesome_script = document.createElement('script');
+                                my_awesome_script.setAttribute('src', '../../functions/orientation.js');
+                                // my_awesome_script.src = "../functions/orientation.js";
+                                document.body.appendChild(my_awesome_script);
                                 document.getElementById("startButton").style.display = "none";
-                                studySessionData.doneDay3 = "stratDayThree";
+                                studySessionData.doneTest2 = "stratTest2";
                                 platform.saveSession(studySessionData, true);
                                 getMillisec();
-                                // msCount();
                                 let startIntervalTest = async function () {
                                     let startTrainning = await startInterval2Tests2();
+                                    // button = randDevButton();
                                     if (startTrainning == "done3") {
+                                        document.getElementById('startDevtestButtonRed').style.display = "inline";
+                                        document.getElementById("iframe-element").style.top = "1%"
+                                        document.getElementById("iframe-element").src = "../../timer/timer.html";
+                                        document.getElementById('iframe-element').classList.remove('hidden');
+                                        document.getElementById("iframe-element").style.display = "inline";
                                         setTimeout(() => {
-                                            document.getElementById("startDevtestButtonRed").style.display = "inline";
-                                            document.getElementById("iframe-element").style.top = "1%"
-                                            document.getElementById("iframe-element").src = "../../timer/timer.html";
-                                            document.getElementById('iframe-element').classList.remove('hidden');
-                                            document.getElementById("iframe-element").style.display = "inline";
-                                        }, 1000)
-                                        setTimeout(() => {
-                                            document.getElementById("startDevtestButtonRed").style.display = "none";
+                                            document.getElementById('startDevtestButtonRed').style.display = "none";
                                             document.getElementById("iframe-element").style.display = "none";
                                             let startDevaluation = async function () {
-                                                let doneDayThree = await startDevTest(); // add promise and resolve
-                                                if (doneDayThree == "doneDevTest") {
-                                                    studySessionData.doneDay3 = "doneDevTest";
-                                                    studySessionData.expDaysDate = updatedDates.fullDate;
-                                                    platform.saveSession(studySessionData, true)
-                                                    document.getElementById("blueButton").style.display = "none";
+                                                let doneTest2 = await startDevTest(); // add promise and resolve
+                                                if (doneTest2 == "doneTest2") {
                                                     document.getElementById("redButton").style.display = "none";
+                                                    document.getElementById("blueButton").style.display = "none";
+                                                    studySessionData.doneTest2 = "doneTest2";
+                                                    studySessionData.expDaysDate = updatedDates.fullDate;
+                                                    platform.saveSession(studySessionData, true);
+                                                    reset_blueCar()
+                                                    reset_redCar()
                                                     sumCorrectFirstPress().then((sum) => {
                                                         showWinningsEnd(sum);
                                                         platform.saveSession(totalWins, true);
@@ -80,7 +81,7 @@ function timeline() {
                                 startIntervalTest();
                             }
                         }
-                        goTwo()
+                        goFour()
                     }
                 } else {
                     if (window.matchMedia("(orientation: landscape)").matches) {
@@ -92,5 +93,5 @@ function timeline() {
             });
         });
     }
-    startDayTwo();
+    startDayFour();
 }
