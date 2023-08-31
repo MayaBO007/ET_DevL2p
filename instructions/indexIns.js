7936cd724e4991e4fbd3e53ebc21ed16e4dce44d
Maya <mayabaror@gmail.com>
2023-08-31 16:37:15 +0300

    ss

--- instructions/indexIns.js
+++ instructions/indexIns.js
@@ -1,14 +1,15 @@
 
 // move to main function
 function timeline() {
-    let updatedDates = updateDates();
-    studySessionData.startDate = startDate;
     let goIns = async function () {
-        let doneInstructions = await startFirstDay();
         studySessionData.doneInstructions = "stratIns";
+        platform.saveSession(studySessionData, true);
+        let doneInstructions = await startFirstDay();
         if (doneInstructions == "doneInstructions") {
+            let updatedDates = updateDates();
             studySessionData.doneInstructions = "doneInstructions";
             studySessionData.expDaysDate = updatedDates.fullDate;
+            studySessionData.startDate = startDate;
             platform.saveSession(studySessionData, true);
             platform.goToUrl("instructions/questions/Multiple-Choice-Quiz-JavaScript-master/index.html");
         }
