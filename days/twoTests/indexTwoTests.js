5e5bf03f88a88db860f1b64f8853f08b31baee43
Maya <mayabaror@gmail.com>
2023-09-01 15:30:39 +0300

    ss

--- days/twoTests/indexTwoTests.js
+++ days/twoTests/indexTwoTests.js
@@ -7,53 +7,53 @@ function timeline() {
             studySessionData = data[indexI];
             deleteFromSessionData();
             let updatedDates = updateDates();
-            if (updatedDates.fullDate.getDate() == updatedDates.yesterday.getDate()) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {
-                if (window.matchMedia("(orientation: landscape)").matches) {
-                    document.getElementById("fiveAM").style.display = "inline";
-                } else {
-                    document.getElementById("fiveAM_hor").style.display = "inline";
+            // if (updatedDates.fullDate.getDate() == updatedDates.yesterday.getDate()) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {
+            //     if (window.matchMedia("(orientation: landscape)").matches) {
+            //         document.getElementById("fiveAM").style.display = "inline";
+            //     } else {
+            //         document.getElementById("fiveAM_hor").style.display = "inline";
+            //     }
+            //     setTimeout(() => {
+            //         moveToDay();
+            //     }, timeToFive());
+            // }
+            // else if (updatedDates.fullDate.getDate() == updatedDates.yesterdayPlusOne.getDate()) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {)
+            //     if (0 <= updatedDates.fullDate.getHours() & updatedDates.fullDate.getHours() < 5) {
+            //         document.getElementById("fiveAM").style.display = "inline";
+            //         setTimeout(() => {
+            //             moveToDay();
+            //         }, timeToFiveSameDay());
+            //     } else {
+            let startTwoTests = async function () {
+                let doneTest1 = await start2tests(); // add promise and resolve
+                if (doneTest1 == "done") {
+                    studySessionData.doneTest1 = 'doneTest1';
+                    studySessionData.expDaysDate = updatedDates.fullDate;
+                    platform.saveSession(studySessionData, true)
+                    document.getElementById("endDayMsg").style.display = "inline";
+                    document.getElementById("endDayMsg").addEventListener("click", function () {
+                        showWinnings()
+                        setTimeout(() => {
+                            platform.goToUrl("days/devTest/devTest.html");
+                        }, timeToFive())
+                        setTimeout(() => {
+                            hideWinnings();
+                            if (window.matchMedia("(orientation: landscape)").matches) {
+                                document.getElementById("fiveAM").style.display = "inline";
+                            } else {
+                                document.getElementById("fiveAM_hor").style.display = "inline";
+                            }
+                        }, 10000);
+                    })
                 }
-                setTimeout(() => {
-                    moveToDay();
-                }, timeToFive());
-            }
-            else if (updatedDates.fullDate.getDate() == updatedDates.yesterdayPlusOne.getDate()) { //|| yesterdayPlusOne.getDate() - fullDate.getDate() > 25 ) {)
-                if (0 <= updatedDates.fullDate.getHours() & updatedDates.fullDate.getHours() < 5) {
-                    document.getElementById("fiveAM").style.display = "inline";
-                    setTimeout(() => {
-                        moveToDay();
-                    }, timeToFiveSameDay());
-                } else {
-                    let startTwoTests = async function () {
-                        let doneTest1 = await start2tests(); // add promise and resolve
-                        if (doneTest1 == "done") {
-                            studySessionData.doneTest1 = 'doneTest1';
-                            studySessionData.expDaysDate = updatedDates.fullDate;
-                            platform.saveSession(studySessionData, true)
-                            document.getElementById("endDayMsg").style.display = "inline";
-                            document.getElementById("endDayMsg").addEventListener("click", function () {
-                                showWinnings()
-                                setTimeout(() => {
-                                    platform.goToUrl("days/devTest/devTest.html");
-                                }, timeToFive())
-                                setTimeout(() => {
-                                    hideWinnings();
-                                    if (window.matchMedia("(orientation: landscape)").matches) {
-                                        document.getElementById("fiveAM").style.display = "inline";
-                                    } else {
-                                        document.getElementById("fiveAM_hor").style.display = "inline";
-                                    }
-                                }, 10000);
-                            })
-                        }
 
-                    }
-                    startTwoTests()
-                }
-                // }
-            } else {
-                document.getElementById("endOfGame").style.display = "inline";
             }
+            startTwoTests()
+            // }
+            // }
+            // } else {
+            //     document.getElementById("endOfGame").style.display = "inline";
+            // }
         })
     })
 }
