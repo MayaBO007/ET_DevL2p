615c0cb377b197c7b8d479784d85a3461830be46
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2024-01-22 13:12:52 +0200

    ss

--- functions/timerPointes.js
+++ functions/timerPointes.js
@@ -195,7 +195,7 @@ function updateDates() {
     let today = getTodayDate();
     if ((studySessionData == undefined) || (studySessionData.doneInstructions == "")) {
         return
-    } else if ((studySessionData.isDayDone == "") && (studySessionData.doneInstructions != "")) {
+    } else if ((studySessionData.isDayDone == "") && (studySessionData.doneInstructions != "") && (fullDate.getDate() - 1 == studySessionData.expDaysDate.getDate())) {
         yesterday = null;
         yesterdayPlusOne = null;
         nextDay = new Date(studySessionData.expDaysDate)
